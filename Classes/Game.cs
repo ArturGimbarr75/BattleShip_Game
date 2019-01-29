4c32a30451531adcda94aeff82cc19dd88179c03
Eonevsg <44285971+Eonevsg@users.noreply.github.com>
2019-01-29 16:41:28 +0200

    Add files via upload

---
+++ Classes/Game.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BattleShip
+{
+    public class Game
+    {
+        public Player Player1 { get; set; }
+        public Player Player2 { get; set; }
+
+        public Game()
+        {
+
+        }
+
+        //
+    }
+}
