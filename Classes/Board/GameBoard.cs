60107420c178671d4736c5d5b0f3934058834d5e
Eonevsg <44285971+Eonevsg@users.noreply.github.com>
2019-01-29 16:41:12 +0200

    Add files via upload

---
+++ Classes/Board/GameBoard.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BattleShip
+{
+    public class GameBoard
+    {
+        const int _NUM_OF_ROWS_ = 10;
+        const int _NUM_OF_COLUMS_ = 10;
+
+        public List<Panel> Panels { get; set; }
+
+        public GameBoard()
+        {
+            Panels = new List<Panel>();
+
+            for (int i = 0; i < _NUM_OF_ROWS_; i++)
+            {
+                for (int j = 0; j < _NUM_OF_COLUMS_; j++)
+                {
+                    Panels.Add(new Panel(i, j));
+                }
+
+            }
+        }
+    }
+}
