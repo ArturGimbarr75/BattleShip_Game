9366f46d845b91027c7a8ea288ba69dff1f4690f
Eonevsg <44285971+Eonevsg@users.noreply.github.com>
2019-01-29 16:40:14 +0200

    Add files via upload

---
+++ Classes/Ship.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BattleShip
+{
+    public class Ship
+    {
+        public string Name { get; set; }
+        public int Width { get; set; }
+        public int Hits { get; set; }
+        public OccupationType OccupationType;
+        public bool IsSunk 
+            {
+
+            get
+                {
+                return Hits >= Width;
+                }
+                
+            }
+
+    }
+
+    public class BattleShip : Ship
+    {
+        public BattleShip()
+        {
+            Name = "BattleShip";
+            Width = 4;
+            OccupationType = OccupationType.BattleShip;
+        }
+    }
+
+    public class Cruiser : Ship
+    {
+        public Cruiser()
+        {
+            Name = "Cruiser";
+            Width = 3;
+            OccupationType = OccupationType.Cruiser;
+        }
+    }
+
+    public class Destroyer : Ship
+    {
+        public Destroyer()
+        {
+            Name = "Destroyer";
+            Width = 2;
+            OccupationType = OccupationType.Destroyer;
+        }
+    }
+
+    public class Submarine : Ship
+    {
+        public Submarine()
+        {
+            Name = "Submarine";
+            Width = 1;
+            OccupationType = OccupationType.Submarine;
+        }
+    }
+
+}
