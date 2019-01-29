f179e5b81b471c6beefc09db0434468157240ba1
Eonevsg <44285971+Eonevsg@users.noreply.github.com>
2019-01-29 16:43:45 +0200

    Add files via upload

---
+++ Classes/Player.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BattleShip
+{
+    public class Player
+    {
+        public string Name { get; set; }
+        public GameBoard GameBoard { get; set; }
+        //public FiringBoard FiringBoard { get; set; }
+        public List<Ship> Ships { get; set; }
+        //public bool HasLost
+
+        public Player (string name)
+        {
+            Name = name;
+            Ships = new List<Ship>()
+            {
+                new BattleShip(),
+                new Cruiser(),
+                new Destroyer(),
+                new Submarine()
+        
+            };
+
+            GameBoard = new GameBoard();
+        }
+    }
+}
